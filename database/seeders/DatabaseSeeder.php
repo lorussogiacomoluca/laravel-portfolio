da3e4b34c9204889e5f17f9365174eface14e8b7
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-10-08 19:52:49 +0200

    Model, Migration and Seeder for Categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(ProjectTableSeeder::class);
+        $this->call([
+            CategoriesTableSeeder::class,
+            ProjectTableSeeder::class
+        ]);
     }
 }
